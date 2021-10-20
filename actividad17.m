1bc6196ebc3b00c61ff6f311ebd8703259823c79
aviescobar <91568563+aviescobar@users.noreply.github.com>
2021-10-20 15:12:18 -0500

    Scrip para la actividad 17
    comentario para mejorar la actividad 17

--- actividad17.m
+++ actividad17.m
@@ -1,29 +1,31 @@
 % Octave Script
 % Title            :Actividad 17 Funcion minimo material problema v2
 % Descrption       :Script para la graficacion del problema de la alberca
-% Authors          :Erick Gabriel Garcia, Alejandro Garcia Cruz
+% Authors          :Erick Gabriel Garcia, Alejandro Garcia Cruz, Victoria Avigail Escobar Noguez 
 % Date             :202123419
 % Version          :1
 % Usage			         :octave> /path/actividad17
 % Notes			         :Se requiere aplicacion Octave
 
-%limpiar variables 
+%limpiar varaibles 
  clear
-%titulo
-disp ('Problema V2 ¨La alberca¨');
-%Rango
-b=0:0.01:10;
-%Valor de la funcion
-ar=@(r) (80)/(r)+(2*r);
-%Funcion a plotear
-fun=(80/x)+(x*2);
-%Funcion para determinar valor minimo
-b= fminbnd(ar,0,10);
-a= (40)/6.324;
-%graficar
-ezplot((80/x)+(x*2));
-disp("Podemos deducir que el valor de los lados es: "), b, a
-
+%Ejemplo minimo de material para envase cilindrico
+%Rango de 0..1 en i=0.1
+ r=0:0.1:1;
+%Valor de la funsion 
+  ar=@(r) (80)/(r)+(2*r);
+%funcion a plotear
+ezplot"((80/x)+(x*2))"
+%funsion para determinar valor minimo
+resr = fminbnd(ar,0,1);
+%Dibujar
+ plot"(r, y");
+%Titulo
+ title(['Minimo material r = ' num2str(resr)]);
+%Etiqueta para x
+ xlabel(['Minimo r = ' num2str(resr)]);
+%Etiqueta para y
+ ylabel(['Min y = ' num2str( ar(resr))]);
 
 
 
